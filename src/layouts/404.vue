24fc3f25682f6b42076c5b07bdce24ab60ab9b83
mfranken <miguel.franken@denkwerk.com>
2023-02-23 17:10:16 +0100

    Add 404 component page stub

--- src/layouts/404.vue
+++ src/layouts/404.vue
@@ -4,15 +4,22 @@ const { t } = useI18n()
 </script>
 
 <template>
-  <main p="x4 y10" text="center teal-700 dark:gray-200">
-    <div text-4xl>
-      <div i-carbon-warning inline-block />
-    </div>
-    <RouterView />
-    <div>
-      <button btn text-sm m="3 t8" @click="router.back()">
-        {{ t('button.back') }}
-      </button>
+  <main class="grid min-h-full place-items-center bg-white py-24 px-6 sm:py-32 lg:px-8">
+    <div class="text-center">
+      <p class="text-base font-semibold text-indigo-600">
+        404
+      </p>
+      <h1 class="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+        Page not found
+      </h1>
+      <p class="mt-6 text-base leading-7 text-gray-600">
+        <RouterView />
+      </p>
+      <div class="mt-10 flex items-center justify-center gap-x-6">
+        <button class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" @click="router.back()">
+          {{ t('button.back') }}
+        </button>
+      </div>
     </div>
   </main>
 </template>
